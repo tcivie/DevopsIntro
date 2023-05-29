157dba21232426f541e7c47974f84975f0c1357b
Gleb Tcivie <gleb@tcivie.com>
2023-05-29 13:25:41 +0300

    Added small popup for to show if the input was successful

--- public/register.js
+++ public/register.js
@@ -18,7 +18,18 @@ document.getElementById('register-form').addEventListener('submit', function(e) 
         },
         body: JSON.stringify(data)
     })
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error('Error:', error));
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+        alert('Registration successful!');
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('Registration failed!');
+    });
 });
